25f4f0e11d5563b92b89dc6fcd92e6e50d6e32e6
imad mimouni <imad.mimouni.123@gmail.com>
2024-04-09 14:10:13 +0000

    dockerrizing the backend

--- back-end/prisma/migrations/20240328213552_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `redirectUrl` on the `Question` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Question" DROP COLUMN "redirectUrl";
